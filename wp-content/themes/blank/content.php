5d7f42c6b714eb3ca1c679581acb62586fa07597
Jacob Giberson <jacobgiberson@Jacobs-iMac.local>
2017-05-09 22:01:18 -0400

    fixed spacing

--- wp-content/themes/blank/content.php
+++ wp-content/themes/blank/content.php
@@ -2,6 +2,6 @@
   <h2 class="blog-post-title"><?php the_title(); ?></h2>
   <p class="blog-post-meta"><?php the_date(); ?> by <a href="#"><?php the_author(); ?></a></p>
 
- <?php the_content(); ?>
+  <?php the_content(); ?>
 
 </div><!-- /.blog-post -->
